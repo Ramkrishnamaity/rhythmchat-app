b55e383cca68f1dfc8c7de3c0484e344257fba54
Ramkrishna Maity <ramkrishnamaity4146@gmail.com>
2024-11-04 07:21:42 +0530

    Initial Commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAW2FXmhQpZZ6YM-QGIj_llw5agA8bCJ0o",
-  authDomain: "rhythmchat-c36d0.firebaseapp.com",
-  projectId: "rhythmchat-c36d0",
-  storageBucket: "rhythmchat-c36d0.appspot.com",
-  messagingSenderId: "1095864302162",
-  appId: "1:1095864302162:web:86577dd9a9311bdaeafd8b",
-  measurementId: "G-7BQ6JF9P3W"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
